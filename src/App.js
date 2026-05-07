42e77d38aa176f9e8909b8948223d8d381a7cf73
Stephanie <stephaniefranca015@gmail.com>
2026-05-07 14:37:52 -0300

    feat: second challenge solved with tab synchronization

--- src/App.js
+++ src/App.js
@@ -7,15 +7,33 @@ import Layout from './components/Layout';
 import themes from './styles/themes';
 
 function App() {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme || 'dark';
+  });
 
   const currentTheme = useMemo(() => {
     return themes[theme] || themes.dark
   }, [theme]);
 
   function handleToggleTheme() {
-    setTheme(preState => preState == 'dark' ? 'light' : 'dark');
+    setTheme(preState => {
+      const actualTheme = preState === 'dark' ? 'light' : 'dark';
+      localStorage.setItem('theme', actualTheme);
+      return actualTheme;
+    });
   }
+
+  useEffect(() => {
+    const handleStorageChange = (event) => {
+      if (event.key === 'theme' && event.newValue){
+        setTheme(event.newValue);
+      }
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+    return () => window.removeEventListener('storage', handleStorageChange);
+  }, []);
 
   return (
     <ThemeProvider theme={currentTheme}>
